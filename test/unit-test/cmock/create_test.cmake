7696b85ea4b20dcab9f3965646271552679e18c4
Gaurav Aggarwal <aggarg@amazon.com>
2024-08-27 10:18:10 +0000

    Make coverage target work on Linux and Mac
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- test/unit-test/cmock/create_test.cmake
+++ test/unit-test/cmock/create_test.cmake
@@ -127,10 +127,20 @@ function(create_mock_list mock_name
                                ${mocks_dir}
                                ${mock_include_list}
            )
-    set_target_properties(${mock_name} PROPERTIES
-                        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib
-                        POSITION_INDEPENDENT_CODE ON
-            )
+
+    if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        set_target_properties(${mock_name} PROPERTIES
+                              LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib
+                              POSITION_INDEPENDENT_CODE ON
+                              LINK_FLAGS "-Wl,-undefined,dynamic_lookup"
+                             )
+    else()
+        set_target_properties(${mock_name} PROPERTIES
+                              LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib
+                              POSITION_INDEPENDENT_CODE ON
+                             )
+    endif()
+
     target_compile_definitions(${mock_name} PUBLIC
             ${mock_define_list}
         )
